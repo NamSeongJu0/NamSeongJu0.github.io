4a8fee120b6ab2cf21e821a33e02df86bec2eb97
NamSeongJu0 <skaehdwn0@naver.com>
2023-04-10 20:47:19 +0900

    sefsfd
    aazdxfvz

---
+++ jsfile/header_footer.js
@@ -0,0 +1,16 @@
+function updateTime() {
+    let date = new Date();
+
+    let Hours = ('0' + date.getHours()).slice(-2); 
+    let Minutes = ('0' + date.getMinutes()).slice(-2);
+    let Seconds = ('0' + date.getSeconds()).slice(-2); 
+
+    let dstring = Hours + ' : ' + Minutes + ' : ' + Seconds;
+
+    document.getElementsByClassName('time')[0].innerHTML = dstring;
+}
+
+setInterval(updateTime, 1000);
+
+
+
